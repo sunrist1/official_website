2f861bd6538a1fb4b3d875fb3090bfb75138cdd6
sunrist1 <294333575@qq.com>
2017-08-25 18:17:54 +0800

    add transition and nav

--- js/init.js
+++ js/init.js
@@ -4,5 +4,18 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    $('.modal').modal();
+
+    var options = [
+      {selector: '#staggered_first', offset: 200, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      // {selector: '#staggered-test', offset: 50, callback: function(el) {
+      //   Materialize.showStaggeredList($(el));
+      // } },
+    ];
+
+    Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space

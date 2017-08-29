c5c3d0156b71bc1c04d4ca78b9f3327f08ca95ec
sunrist1 <294333575@qq.com>
2017-08-30 00:23:40 +0800

    add something

--- js/init.js
+++ js/init.js
@@ -10,12 +10,23 @@
       {selector: '#staggered_first', offset: 200, callback: function(el) {
         Materialize.showStaggeredList($(el));
       } },
-      // {selector: '#staggered-test', offset: 50, callback: function(el) {
-      //   Materialize.showStaggeredList($(el));
-      // } },
+      {selector: '#staggered-second', offset: 100, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#staggered-third', offset: 100, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
     ];
 
     Materialize.scrollFire(options);
+
+    // 导航栏，用户案例下拉列表
+    $(".dropdown-button").dropdown({
+      hover:true
+    });
+
+    // 图片点击放大
+    // $('.materialboxed').materialbox();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
